4d288bd7d2c8d7101d8ed650c5aabc7130cf8c0c
yuejun.hou <yuejun.hou@rograndec.com>
2016-12-16 14:23:23 +0800

    54151

--- YJGuideView/YJGuideView/YJGuideView/YJGuideView.h
+++ YJGuideView/YJGuideView/YJGuideView/YJGuideView.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, YJGuideViewAnchorType) {
+    YJGuideViewAnchorRect,        //矩形
+    YJGuideViewAnchorRoundRect,  // 圆角矩形
+    YJGuideViewAnchorOval        //椭圆
+};
+
 @interface YJGuideView : UIView
+
+@property (nonatomic, assign) CGRect showRect; /**< 锚点frame */
+@property (nonatomic, assign) YJGuideViewAnchorType showType; /**< 锚点显示类型 */
+@property (nonatomic, assign, getter=isFullShow) BOOL fullShow; /**< 锚点全部显示 */
+@property (nonatomic, strong) UIColor *guideBgColor; /**< 覆盖的颜色 */
+@property (nonatomic, assign, getter=isMarkShow) BOOL markShow; /**< 是否显示提示文本，默认不显示 */
+@property (nonatomic, copy) NSString *markString; /**< 提示文本内容 */
 
 @end
