54accd5f3adab16e23d5c3b08cc0cd5d993011ba
yuejun.hou <11283019@qq.com>
2016-12-17 21:11:56 +0800

    sfdf

--- YJGuideView/YJGuideView/ViewController.m
+++ YJGuideView/YJGuideView/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 #import "YJGuideView.h"
+#import <CoreText/CoreText.h>
+#import <CoreImage/CoreImage.h>
+
 
 @interface ViewController ()
 
@@ -18,12 +21,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
     YJGuideView *view = [[YJGuideView alloc] init];
-    
-    
-    
-    CGRect frame = CGRectMake(100, 100, 1, 1);
+    CGRect frame = CGRectMake(100, 100, 100, 100);
     view.showRect = frame;
+    
+    UILabel *firstLabel = [[UILabel alloc] initWithFrame:frame];
+    firstLabel.font = [UIFont systemFontOfSize:15];
+    firstLabel.textAlignment = NSTextAlignmentLeft;
+    firstLabel.backgroundColor = [UIColor redColor];
+//    [self.view addSubview:firstLabel];
+    
+    
+    CGRect secFrame = [view _scaleFrame:frame ratio:[view ovalDrawScale]];
+    UILabel *secLabel = [[UILabel alloc] initWithFrame:secFrame];
+    secLabel.font = [UIFont systemFontOfSize:15];
+    secLabel.textAlignment = NSTextAlignmentLeft;
+    secLabel.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:0.5];
+//    [self.view addSubview:secLabel];
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    UIBezierPath *fullPath  = [UIBezierPath bezierPathWithRect:frame];
+    UIBezierPath *showPath = [UIBezierPath bezierPathWithOvalInRect:[view _scaleFrame:view.showRect ratio:[view ovalDrawScale]]];
+    CGContextSetFillColorWithColor(context, [UIColor orangeColor].CGColor);
+    [fullPath appendPath:[showPath bezierPathByReversingPath]];
+    CGContextAddPath(context, fullPath.CGPath);
+    CGContextFillPath(context);
+
+
+    
+    
     
     NSLog(@"++++ %f", [view ovalDrawScale]);
 }
