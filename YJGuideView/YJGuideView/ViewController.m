2d456a302ebb084679e7d4ce892a467df4c5fef2
yuejun.hou <yuejun.hou@rograndec.com>
2016-12-19 10:32:39 +0800

    56

--- YJGuideView/YJGuideView/ViewController.m
+++ YJGuideView/YJGuideView/ViewController.m
@@ -7,15 +7,10 @@
 //
 
 #import "ViewController.h"
-#import "YJGuideView.h"
-#import <CoreText/CoreText.h>
-#import <CoreImage/CoreImage.h>
 #import "YJGuideViewController.h"
 
 
-@interface ViewController () {
-    YJGuideView *markView;
-}
+@interface ViewController ()
 
 @end
 
@@ -24,36 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-//    
-//    YJGuideView *view = [[YJGuideView alloc] init];
-//    CGRect frame = CGRectMake(100, 100, 100, 100);
-//    view.showRect = frame;
-//    
-//    UILabel *firstLabel = [[UILabel alloc] initWithFrame:frame];
-//    firstLabel.font = [UIFont systemFontOfSize:15];
-//    firstLabel.textAlignment = NSTextAlignmentLeft;
-//    firstLabel.backgroundColor = [UIColor redColor];
-////    [self.view addSubview:firstLabel];
-//    
-//    
-//    CGRect secFrame = [view _scaleFrame:frame ratio:[view ovalDrawScale]];
-//    UILabel *secLabel = [[UILabel alloc] initWithFrame:secFrame];
-//    secLabel.font = [UIFont systemFontOfSize:15];
-//    secLabel.textAlignment = NSTextAlignmentLeft;
-//    secLabel.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:0.5];
-////    [self.view addSubview:secLabel];
-//    
-//    CGContextRef context = UIGraphicsGetCurrentContext();
-//    UIBezierPath *fullPath  = [UIBezierPath bezierPathWithRect:frame];
-//    UIBezierPath *showPath = [UIBezierPath bezierPathWithOvalInRect:[view _scaleFrame:view.showRect ratio:[view ovalDrawScale]]];
-//    CGContextSetFillColorWithColor(context, [UIColor orangeColor].CGColor);
-//    [fullPath appendPath:[showPath bezierPathByReversingPath]];
-//    CGContextAddPath(context, fullPath.CGPath);
-//    CGContextFillPath(context);
 
-
-    
-    
     UIImageView *imageView = [[UIImageView alloc]initWithFrame:self.view.bounds];
     [imageView setImage:[UIImage imageNamed:@"IMG_4829.PNG"]];
     [self.view addSubview:imageView];
@@ -65,22 +31,8 @@
     labelInit.backgroundColor = [UIColor redColor];
     [self.view addSubview:labelInit];
 
-    
-    CGRect frame = CGRectMake(270, 240, 58, 58);
-    
-    markView = [[YJGuideView alloc]initWithFrame:imageView.bounds];
-    markView.fullShow = YES;
-    markView.showType = YJGuideViewAnchorOval;
-    markView.showRect = frame;
-    [imageView addSubview:markView];
-    
-    
-    
 }
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-//    CGPoint point = [[touches anyObject] locationInView:markView.superview];
-//    markView.showRect = CGRectMake(point.x-markView.showRect.size.width/2.0f, point.y-markView.showRect.size.height/2.0f, markView.showRect.size.width, markView.showRect.size.height);
-    [markView removeFromSuperview];
     
     CGRect frame1 = CGRectMake(270, 240, 58, 58);
     CGRect frame2 = CGRectMake(50, 260, 58, 58);
